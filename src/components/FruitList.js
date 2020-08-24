7d21de6224be7249b8186c95ef64f162b8e60d30
Lauren <elle.rachel.writes@gmail.com>
2020-08-24 12:17:41 -1000

    updated files to listen for and render state updates, and dispatch actions to the store

--- src/components/FruitList.js
+++ src/components/FruitList.js
@@ -3,15 +3,19 @@ import React from 'react';
 class FruitList extends React.Component {
   componentDidMount() {
     // TODO Subscribe to the store to listen for state updates.
+    this.unsubscribe = store.subscribe(()=>{
+      this.forceUpdate()
+    })
   }
 
   componentWillUnmount() {
     // TODO Unsubscribe the listener from the store.
+    if(this.unsubscribe) this.unsubscribe()
   }
 
   render() {
     // TODO Get the fruit state from the store.
-    const fruit = [];
+    const fruit = store.getState();
 
     return (
       <div>
