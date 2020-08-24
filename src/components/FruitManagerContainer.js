38bb57bd51387702d7f566be1851a52991d86c96
Lauren <elle.rachel.writes@gmail.com>
2020-08-24 13:16:20 -1000

    presentational vs container components

--- src/components/FruitManagerContainer.js
+++ src/components/FruitManagerContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import store from '../store';
-import {addFruit} from '../actions/fruitActions';
+import {addFruit, addFruits, sellFruit, sellOut} from '../actions/fruitActions';
 import FruitManager from './FruitManager';
 
 class FruitManagerContainer extends React.Component {
@@ -18,11 +18,24 @@ class FruitManagerContainer extends React.Component {
 
     add = fruit => store.dispatch(addFruit(fruit))
 
+    addBulk = fruit => store.dispatch(addFruits(fruit))
+
+    sell = fruit => store.dispatch(sellFruit(fruit))
+
+    sellAll = () => store.dispatch(sellOut())
+
     render() {
         const { fruit } = store.getState();
+        const distinctFruit = Array.from(new Set(fruit)).sort()
 
         return (
-            <FruitManager fruit={fruit} add={this.add}/>
+            <FruitManager
+                fruit={fruit}
+                distinctFruit={distinctFruit}
+                add={this.add}
+                addBulk={this.addBulk}
+                sell={this.sell}
+                sellAll={this.sellAll} />
         );
     }
 }
