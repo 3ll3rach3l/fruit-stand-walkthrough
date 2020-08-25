06b5972c42288e07a98f7a5ef6451256f63ab702
Lauren <elle.rachel.writes@gmail.com>
2020-08-25 07:37:50 -1000

    added preloaded state

--- src/store.js
+++ src/store.js
@@ -1,6 +1,33 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer'
+import {saveState} from './localStorage';
 
-const store = createStore(rootReducer);
+const preloadedState = loadState();
+
+const store = createStore(rootReducer, preloadedState);
+
+
+store.subscribe(()=>{
+    saveState(store.getState())
+})
+// const preloadedState = {
+//     fruit: [
+//         'APPLE',
+//         'ORANGE',
+//     ],
+//     farmers: {
+//         1: {
+//             id: 1,
+//             name: 'John Smith',
+//             paid: false,
+//         },
+//         2: {
+//             id: 2,
+//             name: 'Sally Jones',
+//             paid: false,
+//         },
+//     }
+// };
+
 
 export default store;
